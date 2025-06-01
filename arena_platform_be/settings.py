f52741deac67a8589edda9e8adf73f5aac82b6f6
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-06-01 15:19:51 +0200

    Refactor user-related functionality: rename 'users' app to 'players', update imports, and add initial models and configurations.

--- arena_platform_be/settings.py
+++ arena_platform_be/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'users',
+    'players',
     'tournaments',
     'analysis',
     'common',
