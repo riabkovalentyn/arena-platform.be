1b4c26c56b0ee85e5a44206992f26e63116715a8
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-06-29 18:33:21 +0200

    Add Player model with fields and admin registration

--- players/models.py
+++ players/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='player_profile')
+    rating = models.FloatField(default=1500.0)
+    games_played = models.PositiveIntegerField(default=0)
+    kills = models.PositiveIntegerField(default=0)
+    deaths = models.PositiveIntegerField(default=0)
+    headshots = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} (Rating: {self.rating})"
+    
+    @property
+    def kd_ratio(self):
+        return round(self.kills / self.deaths, 2) if self.deaths > 0 else self.kills
+
